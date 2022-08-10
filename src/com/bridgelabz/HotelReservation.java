d0a05b0753b5f8d80c1dabf79401b1d6df76bfde
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-11 00:21:53 +0530

    Added best rated hotel for rewward Customer

--- src/com/bridgelabz/HotelReservation.java
+++ src/com/bridgelabz/HotelReservation.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
+class InvalidDetailsException extends Exception
+{
+	InvalidDetailsException (String giveMessage){
+		System.out.println(giveMessage); 
+	}
+}
+
 class HotelData{
 	String hotelName;
 	int hotelRatesOnForWeekdaysRegularCustomer;
@@ -46,43 +53,83 @@ public class HotelReservation {
 		System.out.println(hotels);
 	}
 
-	// public static void findCheapHotelsAsPerGivenDates() {
-	// 	Calender c = new Calender();
-	// 	int price = 0;
-	// 	System.out.println("How many days you want to stay");
-	// 	Scanner sc = new Scanner(System.in);
-	// 	int n = sc.nextInt();
-	// 	int min=0;
-	// 	int hotelRating=0;
-	// 	String hName = " ";
-	// 	for(int j=0;j<n;j++) {			
-	// 		String day = c.getDayFromDate();
-	// 		System.out.println(day);
-	// 		if(day.equals("SUNDAY")||day.equals("SATURDAY")) {
-	// 			min = hotels.get(0).hotelRatesOnForWeekendsRegularCustomer;
-	// 			for(int i =0;i<hotels.size();i++) {
-	// 				if (min>=hotels.get(i).hotelRatesOnForWeekendsRegularCustomer) {
-	// 					min =hotels.get(i).hotelRatesOnForWeekendsRegularCustomer;
-	// 					hName=hotels.get(i).hotelName;
-	// 					hotelRating=hotels.get(i).ratingOfHotel;
-	// 				}
-	// 			}			
-	// 		}
-	// 		else {
-	// 			min = hotels.get(0).hotelRatesOnForWeekdaysRegularCustomer;
-	// 			for(int i =0;i<hotels.size();i++) {
-	// 				if (min>=hotels.get(i).hotelRatesOnForWeekdaysRegularCustomer) {
-	// 					min =hotels.get(i).hotelRatesOnForWeekdaysRegularCustomer;
-	// 					hName=hotels.get(i).hotelName;
-	// 					hotelRating=hotels.get(i).ratingOfHotel;
-	// 				}
-	// 			}
-	// 		}
-	// 		System.out.println(hName+" total price is "+min);	
-	// 		price += min;	
-	// 	}			
-	// 	System.out.println(hName+",Rating: "+ hotelRating +" total price is "+price);	
-	// }
+	public static void findCheapHotelsAsPerGivenDates() {
+		Calender c = new Calender();
+		int price = 0;
+		System.out.println("How many days you want to stay");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int min=0;
+		int hotelRating=0;
+		String hName = " ";
+		for(int j=0;j<n;j++) {			
+			String day = c.getDayFromDate();
+			System.out.println(day);
+			if(day.equals("SUNDAY")||day.equals("SATURDAY")) {
+				min = hotels.get(0).hotelRatesOnForWeekendsRegularCustomer;
+				for(int i =0;i<hotels.size();i++) {
+					if (min>=hotels.get(i).hotelRatesOnForWeekendsRegularCustomer) {
+						min =hotels.get(i).hotelRatesOnForWeekendsRegularCustomer;
+						hName=hotels.get(i).hotelName;
+						hotelRating=hotels.get(i).ratingOfHotel;
+					}
+				}			
+			}
+			else {
+				min = hotels.get(0).hotelRatesOnForWeekdaysRegularCustomer;
+				for(int i =0;i<hotels.size();i++) {
+					if (min>=hotels.get(i).hotelRatesOnForWeekdaysRegularCustomer) {
+						min =hotels.get(i).hotelRatesOnForWeekdaysRegularCustomer;
+						hName=hotels.get(i).hotelName;
+						hotelRating=hotels.get(i).ratingOfHotel;
+					}
+				}
+			}
+			System.out.println(hName+" total price is "+min);	
+			price += min;	
+		}			
+		System.out.println(hName+",Rating: "+ hotelRating +" total price is "+price);	
+	}
+
+	public static void findCheapBestRatedHotelsForRewardCustomerAsPerGivenDates(){
+		Calender c = new Calender();
+		int finalPrice = 0;
+		int price=0;
+		System.out.println("How many days you want to stay");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int max=0;
+		String hName = " ";
+		for(int j=0;j<n;j++) {			
+			String day = c.getDayFromDate();
+			System.out.println(day);
+			if(day.equals("SUNDAY")||day.equals("SATURDAY")) {
+				max = hotels.get(0).ratingOfHotel;
+				for(int i =0;i<hotels.size();i++) {
+					if (max<=hotels.get(i).ratingOfHotel) {
+						max=hotels.get(i).ratingOfHotel;
+						hName=hotels.get(i).hotelName;
+						price=hotels.get(i).hotelRatesOnWeekendsRewardCustomer;
+					}
+				}
+				System.out.println(price);			
+			}
+			else {
+				max = hotels.get(0).ratingOfHotel;
+				for(int i =0;i<hotels.size();i++) {
+					if (max<=hotels.get(i).ratingOfHotel) {
+						max=hotels.get(i).ratingOfHotel;
+						hName=hotels.get(i).hotelName;
+						price=hotels.get(i).hotelRatesOnWeekdaysRewardCustomer;
+					}
+				}
+				System.out.println(price);	
+			}
+			finalPrice = finalPrice + price;
+			System.out.println(hName+" total price is "+finalPrice);					
+		}			
+		System.out.println(hName+",Rating: "+ max +" total price is "+finalPrice);
+	}
 
 	public static void findBestRatedHotelsAsPerGivenDates() {
 		Calender c = new Calender();
@@ -128,7 +175,13 @@ public class HotelReservation {
 	public static void main(String[] args) {		
 		//displayListOfAllhotels();
 		//findCheapHotelsAsPerGivenDates();
-		findBestRatedHotelsAsPerGivenDates();
-	
+		//findBestRatedHotelsAsPerGivenDates();
+		//Expection Handling
+		try {
+			findCheapBestRatedHotelsForRewardCustomerAsPerGivenDates();
+		}
+		catch(Exception e) {
+			System.out.println("Enter the proper input");
+		}
 	}
 }
