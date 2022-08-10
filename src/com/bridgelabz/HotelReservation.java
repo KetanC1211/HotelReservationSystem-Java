a28184db66168bdd53e9e2316fbd1b98e9d3078c
KetanC1211 <ketanchindarkar12@gmail.com>
2022-08-10 21:30:44 +0530

    Added rates For weekdays and weekends

--- src/com/bridgelabz/HotelReservation.java
+++ src/com/bridgelabz/HotelReservation.java
@@ -12,10 +12,10 @@ class HotelData{
 	int hotelRatesOnWeekendsRewardCustomer;
 	int ratingOfHotel;
  
-	public HotelData(String hName,int hratesWeekdayRC,int hRatesOnWeekdaysRC,int hRatesOnWeekdaysRewardCustomer,int hRatesOnWeekendsRewardCustomer,int ratingHotel) {
+	public HotelData(String hName,int hratesWeekdayRC,int hRatesOnWeekendsRC,int hRatesOnWeekdaysRewardCustomer,int hRatesOnWeekendsRewardCustomer,int ratingHotel) {
 		this.hotelName=hName;
 		this.hotelRatesOnForWeekdaysRegularCustomer=hratesWeekdayRC;
-		this.hotelRatesOnForWeekendsRegularCustomer=hRatesOnWeekdaysRC;
+		this.hotelRatesOnForWeekendsRegularCustomer=hRatesOnWeekendsRC;
 		this.hotelRatesOnWeekdaysRewardCustomer=hRatesOnWeekdaysRewardCustomer;
 		this.hotelRatesOnWeekendsRewardCustomer=hRatesOnWeekendsRewardCustomer;
 		this.ratingOfHotel=ratingHotel;
@@ -33,9 +33,9 @@ class HotelData{
 
 public class HotelReservation {
 	
-	static HotelData lakewood = new HotelData("Lakewood",110,80,90,80,3);
-	static HotelData Bridgewood = new HotelData("Bridgewood",160,110,60,50,4);
-	static HotelData Ridgewood = new HotelData("Ridgewood",220,100,150,40,5);
+	static HotelData lakewood = new HotelData("Lakewood",110,90,80,80,3);
+	static HotelData Bridgewood = new HotelData("Bridgewood",160,60,110,50,4);
+	static HotelData Ridgewood = new HotelData("Ridgewood",220,150,100,40,5);
 	public static ArrayList<HotelData> hotels = new  ArrayList<>();
 	static {
 	hotels.add(lakewood);
@@ -52,37 +52,39 @@ public class HotelReservation {
 		System.out.println("How many days you want to stay");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		int min=0;
 		String hName = " ";
 		for(int j=0;j<n;j++) {			
 			String day = c.getDayFromDate();
 			System.out.println(day);
 			if(day.equals("SUNDAY")||day.equals("SATURDAY")) {
-				int min = hotels.get(0).hotelRatesOnForWeekendsRegularCustomer;
+				min = hotels.get(0).hotelRatesOnForWeekendsRegularCustomer;
 				for(int i =0;i<hotels.size();i++) {
 					if (min>=hotels.get(i).hotelRatesOnForWeekendsRegularCustomer) {
 						min =hotels.get(i).hotelRatesOnForWeekendsRegularCustomer;
 						hName=hotels.get(i).hotelName;
 					}
 				}			
-				price  += min;
 			}
 			else {
-				int min = hotels.get(0).hotelRatesOnForWeekdaysRegularCustomer;
+				min = hotels.get(0).hotelRatesOnForWeekdaysRegularCustomer;
 				for(int i =0;i<hotels.size();i++) {
 					if (min>=hotels.get(i).hotelRatesOnForWeekdaysRegularCustomer) {
 						min =hotels.get(i).hotelRatesOnForWeekdaysRegularCustomer;
 						hName=hotels.get(i).hotelName;
 					}
 				}
-				price  += min;
-			}		
+			}
+			System.out.println(hName+" total price is "+min);	
+			price += min;	
 		}
-		System.out.println(hName+" total price is "+price);	
+			
+		System.out.println("total price is "+price);	
 	}
 		
 	public static void main(String[] args) {		
 		displayListOfAllhotels();
-		//findCheapHotelsAsPerGivenDates();
+		findCheapHotelsAsPerGivenDates();
 	
 	}
 }
